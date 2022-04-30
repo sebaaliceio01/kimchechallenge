222ede99072cc0a8d1eb6c4a257eb9712b95da99
sebaaliceio <sebastianaliceio@gmail.com>
2022-04-30 16:53:44 -0300

    add gitignore & countryForm, header, and implement material-ui

---
+++ src/components/CountryForm.jsx
@@ -0,0 +1,13 @@
+import ApolloClient, { gql } from "apollo-boost";
+
+import React from "react"
+
+function countryForm () {
+    return (
+        <div className="countryForm">
+as
+        </div>
+    )
+}
+
+export default countryForm
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function header() {
+
+  return (
+    <div className="header">
+     
+    </div>
+  );
+}
+
+export default header;
